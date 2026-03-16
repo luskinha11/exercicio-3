fc77d7494ff1082eb3bc29667b3b061eb27d89bd
LUCAS ANDRIESKI AMARAL <03259300074@senacrs.edu.br>
2026-03-16 16:11:54 -0300

    dividir

--- calculadora.js
+++ calculadora.js
@@ -2,18 +2,18 @@ function somar (a, b){
     return (a + b)
 }
 console.log(somar (5, 6))
-
+//---------------------------------
 function subtrair (a, b){
     return (a - b)
 }
 console.log(subtrair (10, 20))
-
+//----------------------------------------------
 function multiplicar (a, b){
     return (a * b)
 }
 console.log(multiplicar (50, 100))
-
+//----------------------------------------
 function divisão (a, b){
-    return (a / b)
+    return a / b;
 }
 console.log(divisão (100, 500))
